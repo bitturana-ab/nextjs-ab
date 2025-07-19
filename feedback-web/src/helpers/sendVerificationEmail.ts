fd2b86e09204a6126c05a46a718208603f2c84f8
bitturana-ab <b2ra830@gmail.com>
2025-07-19 10:48:20 +0530

    add resend email,apiresponse,email tsx

--- feedback-web/src/helpers/sendVerificationEmail.ts
+++ feedback-web/src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,26 @@
 import { resend } from "@/lib/resend";
 
+import VerificationEmail from "../../emails/verificationEmail";
+
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    emails:string,
+    username:string,
+    verifyCode:string,
+):Promise<ApiResponse>{
+    try {
+
+        await resend.emails.send({
+            from: "bittu@gmail.com",
+            to:emails,
+            subject:"Any ab | verification code",
+            react:VerificationEmail({username,otp:verifyCode}),
+        });
+
+        return {success:true,message:"verification code send successfully."}
+    } catch (emailError) {
+        console.error("Error sending verification email.",emailError);
+        return {success:false,message:"Failed to send verification email!"}
+    }
+};
